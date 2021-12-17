b4c68d61aa01a2db47703313628b74662af9680d
Keerthana Maheshwaram <kee@gmail.com>
2021-12-18 01:23:22 +0530

    commit

--- SingleDimensionArray.java
+++ SingleDimensionArray.java
@@ -6,6 +6,31 @@ public class SingleDimensionArray {
    
    public SingleDimensionArray(int sizeofarray){
        arr=new int[sizeofarray];
-        
+        for(int i=0;i<arr.length;i++){
+            arr[i]=Integer.MIN_VALUE;
+        }
+   }
+
+   public void insert(int location, int valuetobeinserted){
+       try{
+           if (arr[location]==Integer.MIN_VALUE){
+               arr[location]=valuetobeinserted;
+               System.out.println("successfully inserted");
+           }else{
+               System.out.println("this cell is already occupied");
+           }
+       }catch(ArrayIndexOutOfBoundsException e){
+           System.out.println("invalid index to access array!");
+       }
+   }
+
+   public void ArrayTraversal(){
+       try{
+           for(int i=0;i<arr.length;i++){
+               System.out.print(arr[i]+ " ");
+           }
+       }catch(Exception e){
+           System.out.println("Array no lonnger exists!");
+       }
    }
 }
